c9cfe2a6f3a45286ab0e70fdcf1f2c551fe865aa
Arkajit Datta <arkajitdatta2010@gmai.com>
2022-09-03 23:16:44 +0530

    set up of aws proxy is done

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
+
+'''
+This branch has to be kept seperate from the main branch
+    -> Works as a proxy to the main branch
+'''
+from email import message
+from urllib import response
 from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import logging
 import uvicorn
-import shutil
-import os
-from OCR_proc import do_ocr
-import easyocr
-reader = easyocr.Reader(['en'],gpu = False, model_storage_directory = "Models/.")
+import requests
+import json 
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 logging.basicConfig(
     level=logging.INFO, format="[%(asctime)s] %(levelname)s: %(message)s"
@@ -16,7 +22,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 logging.getLogger("pipeline").setLevel(logging.INFO)
 
-app = FastAPI(title="VIT-Autocaptcha-OCR-functionality", version="1.0.0 beta")
+app = FastAPI(title="VIT-Autocaptcha-OCR-functionality-proxy", version="1.0.0 beta")
 
 #allowing the API to all the specefic orgins
 origins = [
@@ -34,6 +40,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+#request to recieve the image 
+class request_recognise(BaseModel):
+    img: str
+
 @app.get("/")
 def root():
     logging.info("This api is up")
@@ -44,55 +54,43 @@ def root():
         }
     )
 
-@app.post("/doOcr/")
-def ocr(captchaImg: UploadFile = File(...)):
-    path_captcha_storage = "CaptchaImages"
-    
-    if captchaImg != None:
-        captchaImg_filename = captchaImg.filename
-        path_captcha_storage_final = os.path.join(path_captcha_storage, captchaImg_filename)
-        try:
-            logging.info("Storing the Captcha Image")
-            with open(path_captcha_storage_final,"wb") as buffer:
-                shutil.copyfileobj(captchaImg.file, buffer)  
-                logging.info("Captcha saved in storage")   
-        except Exception as e:
-            logging.error(e)
-            logging.error("the file saving process didnt work properly")
+
+@app.post("/aws_proxy")
+def aws_proxy(req: request_recognise):
+    ocr_url = "https://13.127.18.222/recogniseCaptchaBase64/"
+    response = requests.post(ocr_url, verify=False ,json={"img": req.img})
+    print(response.text)
+    if response.ok:
+        result = json.loads(response.text)
+        if "error_message" in result:
+            error_msg = result["error_message"]
+            logging.exception("Error in OCR, Error in the OCR Server")
             return JSONResponse(
-                status_code=400,
+                status_code=404,
                 content={
-                    "message": "Image coulde not be saved properly",
-                    "error_message": -2
+                    "message": "Error in OCR",
+                    "error_message": error_msg
+                }
+            )
+        else:
+            logging.info("Successful OCR in Server Recieved the Results")
+            captcha_res = result["captcha"]
+            return JSONResponse(
+                status_code=200,
+                content={
+                    "captcha": captcha_res,
+                    "message":"Successfull in extracting the captcha"
                 }
             )
     else:
         return JSONResponse(
-            status_code=400,
-            content={
-                "message": "Image not uploaded",
-                "error_message": -1
-            }
-        )
-    try:
-        captcha_res = do_ocr(path_captcha_storage_final, reader=reader)
-    except Exception as e:
-        logging.exception("Error in OCR")
-        return JSONResponse(
-            status_code=404,
-            content={
-                "message": "Error in OCR",
-                "error_message": -4
-            }
-        )
-    return JSONResponse(
-        status_code=200,
-        content={
-            "captcha": captcha_res,
-            "message":"Successfull in extracting the captcha"
-        }
-    )
-    
+                status_code=404,
+                content={
+                    "message": "Error in OCR, response from AWS was not recieved",
+                    "error_message": -6
+                }
+            )
+            
     
 if __name__ == "__main__":
     uvicorn.run(
